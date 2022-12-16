4de0e15a1657da96bbd09f25b39def56e4aaa4f2
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2022-12-16 19:47:03 +0300

    ı changed something

--- src/Card.kt
+++ src/Card.kt
@@ -1,16 +1,16 @@
  class Card {
-    String[] Deck = new String[52];
-    String[] Number = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-    String[] Suit = {"Diamonds","Hearts","Spades","Clubs"}
+    private String[] Deck = new String[52];
+    private String[] Number = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    private String[] Suit = {"Diamonds","Hearts","Spades","Clubs"}
 
-
+//I writed the card's information.
      public static void deck(){
 
          for (int i = 0; i < deck.length ; i++){
 
          deck[i] = number[i % 13] + suit[i / 13];
      }
-
+// I created the deck
          System.out.println();
      }
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+
+        int times , number; // Variables which ı will use.
+
+        Shuffle game = new Shuffle // This shuffles the deck.
+
+        System.out.println("How many times the cards will be given");
+
+        Scanner sc = new Scanner(System.in);
+        times = sc.nextInt();
+        System.out.println("How many cards will be given at one time?");
+        number = sc.nextInt();
+
+
+        for (int i = 1 ; i<= times ; i++){
+
+            System.out.println(i + ":");
+            int sayi[] = game.Shuffle(times);
+
+            for (int j = 0; j< times ; j++){
+
+                int remaining = (sayi[j]%13 ) + 1;
+            }
 
     }
 }
