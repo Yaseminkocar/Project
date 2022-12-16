9c012ca61fe9fc4309ad277d88a5e7fa24d82e49
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2022-12-16 18:17:30 +0300

    changed

---
+++ src/Card.kt
@@ -0,0 +1,7 @@
+ class Card {
+    String[] Deck = new String[52];
+    String[] Number = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+    String[] Suit = {"Diamonds","Hearts","Spades","Clubs"}
+
+
+}
