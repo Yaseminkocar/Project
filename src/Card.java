fc410042fa5694693d3cd60edd642cf3020904df
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2022-12-21 22:06:47 +0300

    I created the deck

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,9 @@
 public class Card {
-public String[] suit ={"Diamonds","Hearts","Spades","Clubs"};
-public String[] number={"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
-int value = 1;
+private String suit ;
+private String number;
+
     public Card(String i, String j) {
+        suit = i;
+        number = j;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Card [] deck = new Card [52];
         String[] suit = new String[0];
         String[] number = new String[0];
        int c=0;
+        suit = new String[]{"Diamonds", "Hearts", "Spades", "Clubs"};
+        number = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+
        for(int i = 0 ; i < suit.length; i++){
            for(int j = 0; j<number.length ; j++) {
            deck[c++] = new Card(suit [i] , number [j]);
