9766895c4fc11e530e42fac634939c175586490e
Yaseminkocar <yasemin.kocar@std.izmirekonomi.edu.tr>
2022-12-24 16:49:43 +0300

    ı tried to write shuffle

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,52 @@
 
-public class Main{
+import java.util.Random;
+
+public class Main {
+
+
     public static void main(String[] args) {
-        Card [] deck = new Card [52];
+        Card[] deck = new Card[52];
         String[] suit = new String[0];
         String[] number = new String[0];
-       int c=0;
+        int c = 0;
         suit = new String[]{"Diamonds", "Hearts", "Spades", "Clubs"};
         number = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
 
-       for(int i = 0 ; i < suit.length; i++){
-           for(int j = 0; j<number.length ; j++) {
-           deck[c++] = new Card(suit [i] , number [j]);
-           System.out.print(suit[i] + " ");
-           System.out.println(number[j]);
+        for (int i = 0; i < suit.length; i++) {
+            for (int j = 0; j < number.length; j++) {
+                deck[c++] = new Card(suit[i], number[j]);
+                System.out.print(suit[i] + " ");
+                System.out.println(number[j]);
 
 
-           }
-           
-       }
-    
-    
+            }
+
+        }
+
+
+
+        public static String Shuffle(Card[] deck){
+            Random r = new Random(System.currentTimeMillis());
+
+            Random rd = new Random();
+            String a = rd.next(suit.length);
+            String b = rd.next(number.length);
+
+
+            System.out.println(suit);
+            System.out.println(number);
+
+        }
     }
+}
 
 
-    
-       }
+
+
+
+
+
+
+
+
+
